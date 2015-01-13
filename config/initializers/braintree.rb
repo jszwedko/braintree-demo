04ac3dc76bcd4f85a08847ef104ae3cda9e69934
jszwedko <jesse.szwedko@gmail.com>
2015-01-13 13:41:35 -0800

    Add braintree gem and initializer

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,6 @@
+require 'braintree'
+
+Braintree::Configuration.environment = ENV['BRAINTREE_ENVIRONMENT'] || :sandbox
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID'] or fail 'BRAINTREE_MERCHANT_ID required in env'
+Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY'] or fail 'BRAINTREE_PUBLIC_KEY required in env'
+Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY'] or fail 'BRAINTREE_PRIVATE_KEY required in env'
